411b172a83705c4954591cd0d2c5b529bcd9aba3
Derrick Johnson <10237736+dj51234@users.noreply.github.com>
2021-02-27 16:18:42 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+function App() {
+  return <h1>My App</h1>;
+}
+
+export default App;
